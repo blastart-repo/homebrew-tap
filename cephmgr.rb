225e067fcb7b1014c73028852037c7a9826ce8a7
vtarmo <vta6722@gmail.com>
2023-10-11 11:32:42 +0300

    Brew formula update for cephmgr version v0.2.0

--- cephmgr.rb
+++ cephmgr.rb
@@ -5,11 +5,11 @@
 class Cephmgr < Formula
   desc ""
   homepage "https://github.com/blastart-repo/cephmgr"
-  version "0.1.3"
+  version "0.2.0"
 
   on_macos do
-    url "https://github.com/blastart-repo/cephmgr/releases/download/v0.1.3/cephmgr_0.1.3_darwin_all.tar.gz"
-    sha256 "bcbba97d683cb15a667a2435b666aade5e64f39bd4ad7adef429a0662e3f5c69"
+    url "https://github.com/blastart-repo/cephmgr/releases/download/v0.2.0/cephmgr_0.2.0_darwin_all.tar.gz"
+    sha256 "3d76a994b3b510941430293bc1338c63027ad148f8797bd63df9f58fd52a5659"
 
     def install
       bin.install "cephmgr"
@@ -18,16 +18,16 @@ class Cephmgr < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/blastart-repo/cephmgr/releases/download/v0.1.3/cephmgr_0.1.3_linux_arm64.tar.gz"
-      sha256 "4f81e5969a66f8e9ffdfcffd8256ec78e27792ee6f78da3c244bc1d1a80941d7"
+      url "https://github.com/blastart-repo/cephmgr/releases/download/v0.2.0/cephmgr_0.2.0_linux_arm64.tar.gz"
+      sha256 "984cce21a4d7d5d664065b4b1feb08e8a8a4df3b2fa1d837a5bfeb48aa59a3ac"
 
       def install
         bin.install "cephmgr"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/blastart-repo/cephmgr/releases/download/v0.1.3/cephmgr_0.1.3_linux_amd64.tar.gz"
-      sha256 "157cbcdf77bc6c44982d4c592faa22c59c27ba35155da1560cd88893f7dcd66f"
+      url "https://github.com/blastart-repo/cephmgr/releases/download/v0.2.0/cephmgr_0.2.0_linux_amd64.tar.gz"
+      sha256 "aafaa62a1bd498f044164a2b5075af3f5291e991a29369fca07b4799f2c1a0d6"
 
       def install
         bin.install "cephmgr"
